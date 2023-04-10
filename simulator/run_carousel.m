838ca7c90cbbc19124f09dce1f46dd1b070451f1
francescodestro <62111706+francescodestro@users.noreply.github.com>
2023-04-10 14:00:51 -0400

    Revert "Manual update"
    This reverts commit 45037df4b100494033901ba9eedb7920a8bdf8af.

--- simulator/run_carousel.m
+++ simulator/run_carousel.m
@@ -8,11 +8,11 @@ rng default
 control_mode= 0; % variable passed to controller.m function, useful to test multiple control strategies
                     % implemented control strategies:
                     % 0: open-loop
-                    % 1: control strategy #3 in Destro et al. (2022); example of end-point controller on temperature (works with u_nominal.V_slurry=3e-6 and cryst_output.conc_slurry=250) 
+                    % 1: control strategy #3 in companion paper; example of end-point controller on temperature (works with u_nominal.V_slurry=3e-6 and cryst_output.conc_slurry=250) 
 disturbance_scenario = 0;  % 0: normal operating conditions; 1: nominal slurry concentration ramp; 2: cake resistance step
 total_duration = 1800; % s
 
-% set nominal operating variables   
+%% set nominal operating variables   
 u_nominal.t_cycle=30;           % nominal set-point of cycle duration (s) 
                               % u_nominal.t_cycle and u.t_cycle MUST ALWAYS BE INTEGERS
 u_nominal.V_slurry=3e-6;     % nominal set-point of fed slurry volume (m3) 
@@ -22,7 +22,7 @@ u_nominal.V_slurry=3e-6;     % nominal set-point of fed slurry volume (m3)
 u_nominal.P_compr=10e4;            % nominal set-point of gauge pressure compressor (Pa)
 u_nominal.Tinlet_drying=50+273.15;      % nominal set-point of drying gas temperature (K) 
 
-% set nominal feed properties
+%% set nominal feed properties
 cryst_output.conc_slurry=250;   % nominal slurry concentration (kg/m3) - 
                                 % actual slurry concentration subject to Gaussian disturbances (+ ramp if disturbance_scenario==1)
 
